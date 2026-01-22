e91c534a94a7ac3cee1bc63506e5ff92e5121e5a
MrGrose <grachev.ro@gmail.com>
2026-01-22 15:34:10 +0300

    Убраны не используемые значения

--- pillstore/app/core/config.py
+++ pillstore/app/core/config.py
@@ -11,8 +11,3 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 PAGINATION_SIZES = [10, 20, 50, 100]
-
-# CATEGORIES = {
-#     1: {"id": 1, "name": "Витамины", "slug": "vitamins", "icon": "fas fa-pills"},
-#     2: {"id": 2, "name": "Протеины", "slug": "proteins", "icon": "fas fa-dumbbell"},
-# }
