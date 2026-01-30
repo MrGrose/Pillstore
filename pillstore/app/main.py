2db6c9842269c873ed26ed73101d9c7ed7bb9cc3
MrGrose <grachev.ro@gmail.com>
2026-01-30 11:59:39 +0300

    Добавлен роутер для ошибок

--- pillstore/app/main.py
+++ pillstore/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 
-from app.routers import products, users, orders, auth, profile, admin
+from app.routers import products, users, orders, auth, profile, admin, errors
 from app.api import api_products
 
 from app.exceptions.products import include_product_exceptions
@@ -18,14 +18,14 @@ async def redirect_to_docs():
     return RedirectResponse(url="/products")
 
 
-app.include_router(auth.auth_router) 
+app.include_router(auth.auth_router)
 app.include_router(api_products.router)
 app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(orders.router)
 app.include_router(admin.router)
 app.include_router(profile.router, prefix="/profile")
-
+app.include_router(errors.router)
 
 
 include_product_exceptions(app)
