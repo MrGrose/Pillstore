293534e74827f9be4a5dc255bc215c3d407ba142
MrGrose <grachev.ro@gmail.com>
2026-01-30 12:44:18 +0300

    Добавлена схема обновления продукта

--- pillstore/app/schemas/product.py
+++ pillstore/app/schemas/product.py
@@ -12,49 +12,80 @@ class ProductSchema(BaseModel):
     price: Decimal
     image_url: str
     stock: int
-    
+
     model_config = ConfigDict(from_attributes=True)
 
 
-
 class ProductCreate(BaseModel):
-    name: str = Field(..., min_length=3, max_length=100, description="Название товара (3-100 символов)")
-    brand: str | None = Field(..., min_length=1, max_length=100, description="Название бренда (1-100 символов)")
-    price: Decimal = Field(..., gt=0, description="Цена товара (больше 0)", decimal_places=2)
-    url: str | None
+    name: str = Field(
+        ...,
+        min_length=3,
+        max_length=100,
+        description="Название товара (3-100 символов)",
+    )
+    name_en: str | None = Field(
+        ..., min_length=0, description="Название товара на анг. (3-100 символов)"
+    )
+    brand: str | None = Field(
+        ...,
+        min_length=1,
+        max_length=100,
+        description="Название бренда (1-100 символов)",
+    )
+    price: Decimal = Field(
+        ..., gt=0, description="Цена товара (больше 0)", decimal_places=2
+    )
+    url: str | None = None
     stock: int = Field(..., ge=0, description="Остаток на складе")
-    category_id: int = Field(..., gt=0)  
-    
+    category_id: list[int] = Field(default_factory=list)
+    is_active: bool
+    description_left: str | None = None
+    description_right: str | None = None
+    image_url: str | None = None
+    seller_id: int
+
     @classmethod
     def as_form(
-            cls,
-            name: Annotated[str, Form(...)],
-            price: Annotated[Decimal, Form(...)],
-            url: Annotated[str | None, Form(...)],
-            stock: Annotated[int, Form(...)],
-            brand: Annotated[str | None, Form()] = None,
-            category_id: Annotated[int, Form(...)] = None,
+        cls,
+        seller_id: Annotated[int, Form(...)],
+        name: Annotated[str, Form(...)],
+        price: Annotated[Decimal, Form(...)],
+        url: Annotated[str | None, Form(...)],
+        stock: Annotated[int, Form(...)],
+        description_left: Annotated[str, Form(...)],
+        description_right: Annotated[str, Form(...)],
+        is_active: Annotated[bool, Form(...)],
+        image_url: Annotated[str | None, Form(...)],
+        brand: Annotated[str | None, Form()] = None,
+        category_id: Annotated[int, Form(...)] = None,
+        name_en: Annotated[str, Form()] = "",
     ) -> "ProductCreate":
         try:
             return cls(
                 name=name,
+                name_en=name_en or "",
                 brand=brand,
                 price=price,
                 url=url,
                 stock=stock,
                 category_id=category_id,
+                description_left=description_left,
+                description_right=description_right,
+                is_active=is_active,
+                image_url=image_url,
+                seller_id=seller_id,
             )
         except ValidationError as e:
             raise RequestValidationError(e.errors())
-        
-        
+
+
 class ProductImport(BaseModel):
     name: str
     name_en: str | None = None
     brand: str | None = None
     price: Decimal
     url: str
-    images: str | None = None 
+    images: str | None = None
     stock: int
     mpn: str | None = None
     category_path: list[str]
@@ -64,8 +95,8 @@ class ProductImport(BaseModel):
 
 class ProductImportList(BaseModel):
     products: list[ProductImport]
-    
-    
+
+
 class ProductRead(BaseModel):
     id: int
     name: str
@@ -73,8 +104,8 @@ class ProductRead(BaseModel):
     price: float
     image_url: str | None = None
     cart_qty: int = 0
-    stock: int = 0 
-    
+    stock: int = 0
+
     model_config = ConfigDict(from_attributes=True)
 
 
@@ -84,7 +115,7 @@ class PageUrls(BaseModel):
     next_url: str
     first_url: str
     last_url: str
-    
+
     model_config = ConfigDict(from_attributes=True)
 
 
@@ -97,7 +128,17 @@ class ProductPagination(BaseModel):
     total_pages: int
     page_size_urls: dict[int, str]
     pagination_sizes: list[int]
-    
+
     model_config = ConfigDict(from_attributes=True)
-    
-    
+
+
+class ProductUpdate(BaseModel):
+    name: str
+    name_en: str = ""
+    brand: str = ""
+    price: float
+    stock: int
+    is_active: bool = True
+    category_ids: list[int] = []
+    description_left: str = ""
+    description_right: str = ""
