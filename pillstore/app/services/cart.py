6c5c5672e3688a8d9cc5453162ada71f9d624d54
MrGrose <grachev.ro@mail.com>
2026-02-18 11:59:51 +0300

    Исправлены импорты

--- pillstore/app/services/cart.py
+++ pillstore/app/services/cart.py
@@ -1,12 +1,11 @@
-
-from sqlalchemy import select, func
-from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.deps import get_db
+from app.core.security import get_current_user
 from app.models.cart_items import CartItem as CartItemModel
 from app.models.users import User as UserModel
-from app.core.security import get_current_user
 
 
 async def get_cart_count(
